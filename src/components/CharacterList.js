366018ae4670618b8b92dad940c5fc4288fa06fe
Maksim V <maks112v@gmail.com>
2019-03-06 15:46:44 -0500

    added index js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,13 +3,25 @@ import React from "react";
 import Character from "./Character";
 
 const CharacterList = props => {
+  console.log(props);
+  if(props.characters === undefined){
+    return(
+      <div>
+        Loading Characters
+      </div>
+    )
+  }
   return (
     <ul>
-      {props.characters.map(character => {
+      {/* {props.characters.map(character => {
         return <Character key={character.name} character={character} />;
-      })}
+      })} */}
     </ul>
   );
 };
+
+const stateToProps = state => {
+  characters: state.characters
+}
 
 export default CharacterList;
